321293f6478d9063a0494509fc99406ec70fcb6d
Michael Christenson II <m3talsmith@gmail.com>
2008-09-16 15:02:19 -0400

    added controllers for tags and tasks. this project is going to be fully ajaxed so the only view we'll see is the general layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 ActionController::Routing::Routes.draw do |map|
+
+# -- Tasks Routes -- #
+  map.resources :tasks
+
+# -- Tag Routes -- #
+  map.resources :tags
   
+# -- General Routes -- #
+  map.root :controller => "tasks"
+
 end
