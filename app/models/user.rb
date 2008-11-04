ab454e35754c294c18da1d99c6ffe393e9be7e4c
Michael Christenson II <m3talsmith@gmail.com>
2008-11-04 17:08:57 -0500

    added a simple user test for basic crud and two named scopes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ActiveRecord::Base
   has_and_belongs_to_many :projects
+  
+  named_scope :active,    :conditions => {:is_active => true}
+  named_scope :inactive,  :conditions => {:is_active => false}
 end
--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -1,9 +1,65 @@
+# create_table "users", :force => true do |t|
+#   t.string   "email"
+#   t.string   "password"
+#   t.boolean  "is_active",  :default => true
+#   t.datetime "created_at"
+#   t.datetime "updated_at"
+# end
+
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
   fixtures :users
   
-  def can_add_user
-    assert true
+  def test_should_list_all_users
+    users = User.find(:all)
+    assert contains_valid_users?(users), "No users were found: #{users}"
   end
+  
+  def test_should_list_all_active_users
+    users = User.active
+    assert contains_valid_users?(users), "No active users were found: #{users}"
+  end
+  
+  def test_should_list_all_inactive_users
+    User.find(:first).update_attributes(:is_active => false)
+    users = User.inactive
+    assert contains_valid_users?(users), "No inactive users were found: #{users}"
+  end
+  
+  def test_should_create_user
+    user = create
+    assert user.valid?, "User was not created: #{user}"
+  end
+  
+  def test_should_find_user
+    user = User.find(:first)
+    assert user.valid?, "User was not found with the id of 1: #{user}"
+  end
+  
+  def test_should_update_user
+    user = User.find(:first)
+    assert user.update_attributes(:is_active => false), "User was not updated: #{user}"
+  end
+  
+  def test_should_destroy_user
+    user = User.find(:first)
+    assert user.destroy, "User was not destroyed: #{user}"
+  end
+  
+  private
+    
+    def create(options={})
+      User.create({}.merge(options))
+    end
+    
+    def is_valid_user?(user)
+      return (user.class.to_s == "User")
+    end
+    
+    def contains_valid_users?(users)
+      user_list = []
+      user_list.push users ? users.first : users.class.to_s == "User"
+      return is_valid_user?(users.first)
+    end
 end
