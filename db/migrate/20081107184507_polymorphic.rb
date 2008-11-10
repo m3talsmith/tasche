c32550042fde185a5aa1a4137d10137868d7e8a0
Michael Christenson II <m3talsmith@gmail.com>
2008-11-10 13:20:08 -0500

    removed some useless migrations

--- db/migrate/20081107184507_polymorphic.rb
+++
@@ -1,9 +0,0 @@
-class Polymorphic < ActiveRecord::Migration
-  def self.up
-    create_table :tag_objects, :force => true do |t| 
-      t.coloumn :name, :string
-      t.coloumn :acquired_at, :datetime
-      t.coloumn :resource_type, :string
-  end
-  
-
