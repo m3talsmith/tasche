ce0f3ad0969e60010726349be98c18f4f9c8e0b5
Bryan <lordbyron@theswift.(none)>
2008-11-24 13:04:34 -0500

    wrote a test for projects

--- test/unit/project_test.rb
+++ test/unit/project_test.rb
@@ -1,8 +1,15 @@
 require 'test_helper'
 
 class ProjectTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  def test_truth
-    assert true
+  fixtures :projects
+  def test_should_list_all_projects
+    projects = Project.find :all
+    assert projects.length > 0,"we couldn't find any projects"
+    assert projects.each {|project| project.is_a? Project}, "we couldn't find a valid project"
+  end
+  def test_project_has_a_user
+    project = Project.find :first
+    assert project.users.length > 0, "we couldn't find any users for a project"
+    assert project.users.each {|user| user.is_a? User}, "couldn't find a valid user"
   end
 end
