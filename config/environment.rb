fe032ed8fbe8e65578ab7c8e03f5d4604ddef5fc
Michael Christenson II <m3talsmith@gmail.com>
2008-11-26 17:10:19 -0500

    added nested routes that follow the model relationships; added gem dependency for authlogic; cleared up comment clutter in environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -1,9 +1,3 @@
-# Be sure to restart your server when you modify this file
-
-# Uncomment below to force Rails into production mode when
-# you don't control web/app server and can't set it the proper way
-# ENV['RAILS_ENV'] ||= 'production'
-
 # Specifies gem version of Rails to use when vendor/rails is not present
 RAILS_GEM_VERSION = '2.1.0' unless defined? RAILS_GEM_VERSION
 
@@ -11,32 +5,12 @@ RAILS_GEM_VERSION = '2.1.0' unless defined? RAILS_GEM_VERSION
 require File.join(File.dirname(__FILE__), 'boot')
 
 Rails::Initializer.run do |config|
-  # Settings in config/environments/* take precedence over those specified here.
-  # Application configuration should go into files in config/initializers
-  # -- all .rb files in that directory are automatically loaded.
-  # See Rails::Configuration for more options.
-
-  # Skip frameworks you're not going to use. To use Rails without a database
-  # you must remove the Active Record framework.
-  # config.frameworks -= [ :active_record, :active_resource, :action_mailer ]
-
   # Specify gems that this application depends on. 
   # They can then be installed with "rake gems:install" on new installations.
   # config.gem "bj"
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
   # config.gem "aws-s3", :lib => "aws/s3"
-
-  # Only load the plugins named here, in the order given. By default, all plugins 
-  # in vendor/plugins are loaded in alphabetical order.
-  # :all can be used as a placeholder for all plugins not explicitly named
-  # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
-
-  # Add additional load paths for your own custom dirs
-  # config.load_paths += %W( #{RAILS_ROOT}/extras )
-
-  # Force all environments to use the same logger level
-  # (by default production uses :info, the others :debug)
-  # config.log_level = :debug
+  config.gem "authlogic"
 
   # Make Time.zone default to the specified zone, and make Active Record store time values
   # in the database in UTC, and return them converted to the specified local zone.
@@ -51,16 +25,6 @@ Rails::Initializer.run do |config|
     :session_key => '_tasche_session',
     :secret      => 'bc156f2c8891bedd93b1e98dd5b480467eeff6a00040ea85a4997f1829382b85ad453bb3d16eeef51424bc949db36ccb88592a3782ae02c1c0173c055ee04261'
   }
-
-  # Use the database for sessions instead of the cookie-based default,
-  # which shouldn't be used to store highly confidential information
-  # (create the session table with "rake db:sessions:create")
-  # config.action_controller.session_store = :active_record_store
-
-  # Use SQL instead of Active Record's schema dumper when creating the test database.
-  # This is necessary if your schema can't be completely dumped by the schema dumper,
-  # like if you have constraints or database-specific column types
-  # config.active_record.schema_format = :sql
 
   # Activate observers that should always be running
   # config.active_record.observers = :cacher, :garbage_collector
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,34 @@
 ActionController::Routing::Routes.draw do |map|
 
+# -- Users Routes -- #
+  map.resources :users do |user|
+    user.resources :projects
+    user.resources :tags
+    user.resources :tasks
+  end
+
+# -- Projects Routes -- #
+  map.resources :projects do |project|
+    project.resources :users
+    project.resources :tags
+    project.resources :tasks
+  end
+
 # -- Tasks Routes -- #
-  map.resources :tasks
+  map.resources :tasks do |task|
+    task.resources :projects
+    task.resources :tags
+    task.resources :users
+  end
 
 # -- Tag Routes -- #
-  map.resources :tags
+  map.resources :tags do |tag|
+    tag.resources :projects
+    tag.resources :users
+    tag.resources :tasks
+  end
   
 # -- General Routes -- #
-  map.root :controller => "tasks"
+  map.root :controller => "users"
 
 end
