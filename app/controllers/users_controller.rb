60783ab8c3cec69b56619a174bb11536b8e994c6
Bryan <lordbyron@theswift.(none)>
2009-02-04 13:53:00 -0500

    created user login for user controller quit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,19 @@
 class UsersController < ApplicationController
+  def authenticate
+    # open_id_user = try_login (params[:user][:openid_identity])
+    if open_id_user.sucess 
+      user = User.find(:first, :conditions => {:identity_url => params[:user][:openid_identity]})
+      if user
+        redirect_to user_url(user.id)
+      else 
+        user = User.create(:identity_url => params[:user][:openid_identity])
+        redirect_to user_profile_url(user.id)
+      end
+    else
+      redirect_to users_open_id_signup_url
+    end
+  end
+  
   def index
   end
   
