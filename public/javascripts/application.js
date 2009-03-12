07b3230a401d16cc435b4888d250e3c0f185d1dc
Michael Christenson II <m3talsmith@gmail.com>
2009-03-12 17:22:28 -0400

    added jquery; starting to add functions for ui effects; added minify to projects;

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,43 @@
-// Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
+function minify_project(project_id)
+{
+  var project_body = $("#" + project_id + " .body");
+  $(project_body).slideUp("normal");
+  $(project_body).addClass("minified");
+}
+
+function minify_projects()
+{
+  $(".project").each(
+    function(i)
+    {
+      minify_project($(this).attr("id"));
+    }
+  );
+}
+
+function expand_project(project_id)
+{
+  var project_body = $("#" + project_id + " .body");
+  $(project_body).slideDown("normal");
+  $(project_body).removeClass("minified");
+}
+
+function init_projects()
+{
+  $(".project").each(
+    function()
+    {
+      $("#" + $(this).attr("id") + ".name").click(
+        function()
+        {
+          if($(this).hasClass("minified")) {expand_project($(this).attr("id"));}
+          else {minify_project($(this).attr("id"));}
+        }
+      );
+    }
+  );
+  
+  minify_projects();
+}
+
+$().ready( function() {init_projects();} );
