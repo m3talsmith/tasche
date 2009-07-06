df0ee95f5c191a5a51c7a1961a971c5f8e40baf2
Michael Christenson II <m3talsmith@gmail.com>
2009-07-06 19:35:26 -0400

    task/user relationship fixed

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,6 +2,7 @@ class Task < ActiveRecord::Base
   has_many                :tags, :as => :taggable
   has_and_belongs_to_many :projects
   has_and_belongs_to_many :users
+  belongs_to              :user
   
   named_scope :order_by_priority, :order => "priority ASC"
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,12 @@ class User < ActiveRecord::Base
   has_and_belongs_to_many :projects
   has_and_belongs_to_many :tasks
   has_many                :tags, :as => :taggable
+  has_many                :tasks
   
   named_scope :active,    :conditions => {:is_active => true}
   named_scope :inactive,  :conditions => {:is_active => false}
   
   def tasks_by_project_id(project_id)
-    Project.find(project_id).tasks.collect {|t| t if t.users.include?(self)}.uniq.compact!
+    Project.find(project_id).tasks.collect { |t| t if t.users.include?(self) }.uniq.compact!
   end
 end
